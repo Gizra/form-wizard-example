1f8a2ba6966a96730fff0e23b8497f14e8ce5dab
Amitai Burstein <amitai@gizra.com>
2020-01-19 23:03:57 +0200

    First commit

---
+++ sites/default/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+// #ddev-generated: Automatically generated Drupal settings file.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
